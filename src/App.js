2b159fb4487944be70b4c56506cd182cb7b5e349
Learned Cat <yudni-sasha@yandex.ru>
2022-09-12 20:54:03 +0300

    Сделал кнопку, по нажатию на которуюотправляется запрос брокеру

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      var url = new URL("http://127.0.0.1:3001/getRandomFood"),
+      params = {latitude:35.696233, longitude:139.570431}
+      Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {err && <h2>{err}</h2>}
+
+      <button onClick={handleClick}>Make request</button>
+
+      {isLoading && <h2>Loading...</h2>}
+
+      {data && (
+        <div>
+          <h2>Data: {JSON.stringify(data, null, 4)}</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
