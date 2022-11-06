76d1289e07b1578f5e72beb1762fc8db6836795a
Learned Cat <yudni-sasha@yandex.ru>
2022-11-06 21:16:49 +0300

    Дозагрузка карточек

--- src/App.js
+++ src/App.js
@@ -8,11 +8,89 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import yandexFoodIcon from './res/yandex_food.ico'
 
+const maxCardsNum = 30;
+const leftCardsNumToRequestNew = 15;
+const cardsNumToRequest = 10;
+var lastLoadedCardIdx = -1;
+var processingCardRequest = false;
+
+function CardIndicesToSet(last, num) {
+  if (last + num > maxCardsNum) {
+      return Array.from({length:maxCardsNum-last-1},(v,k)=>k+last+1).concat(Array.from({length:num+last-maxCardsNum+1},(v,k)=>k));
+  } else {
+      return Array.from({length:num},(v,k)=>k+last+1);
+  }
+}
+
+async function RequestCards(cardsNum) {
+  try {
+    var url = new URL("http://127.0.0.1:3001/getRandomFood"),
+    params = {latitude:55.696233, longitude:37.570431, cardsNum:cardsNum, getTags:true}
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    //console.log('result is: ', JSON.stringify(result, null, 4));
+
+    var foodCards = result["foodCards"];
+    var indicesToSet;
+    if (lastLoadedCardIdx < 0) {
+      indicesToSet = CardIndicesToSet(maxCardsNum+1, foodCards.length);
+      lastLoadedCardIdx = 0;
+    } else {
+      indicesToSet = CardIndicesToSet(lastLoadedCardIdx, foodCards.length);
+    }
+    lastLoadedCardIdx = (lastLoadedCardIdx + foodCards.length - 1) % maxCardsNum;
+    for (var i = 0; i < foodCards.length; i++){
+      var card = foodCards[i];
+      var foodImageWithDuplicates = document.getElementsByName("image" + indicesToSet[i].toString());
+      var foodTitleWithDuplicates = document.getElementsByName("title" + indicesToSet[i].toString());
+      var foodDescriptionWithDuplicates = document.getElementsByName("description" + indicesToSet[i].toString());
+      var foodUrlWithDuplicates = document.getElementsByName("url" + indicesToSet[i].toString());
+      for (var j = 0; j < foodImageWithDuplicates.length; j++) {
+        var foodImg = foodImageWithDuplicates[j];
+        if (foodImg != null) {            
+          var imageUrl = card["imageUrl"];
+          if (!imageUrl.includes("{w}")) {
+            continue;
+          } else {
+            imageUrl = imageUrl.replace("{w}", "200");
+            imageUrl = imageUrl.replace("{h}", "200");
+            foodImg.src = imageUrl;  
+          }
+        }
+        var foodTitle = foodTitleWithDuplicates[j];
+        if (foodTitle != null) {
+          foodTitle.innerText = card["name"];
+        }
+        var foodDescription = foodDescriptionWithDuplicates[j];
+        if (foodDescription != null) {
+          foodDescription.innerText = card["description"];
+        }
+        var foodUrl = foodUrlWithDuplicates[j];
+        if (foodUrl != null) {
+          foodUrl.href = card["restarauntUrl"];
+        }
+      }
+    }
+  } catch (err) {
+    console.log('err: ', err)
+  }
+  processingCardRequest = false;
+}
+
 export default function App() {
-
-  const cardsNum = 10;
-  var initialized = false;
-
   const settings = {
     infinite: true,
     speed: 500,
@@ -24,78 +102,21 @@ export default function App() {
     centerPadding: "60px",
     slidesToShow: 5,
     adaptiveHeight: true,
-    //initialSlide: 3,
-    onReInit: () => {
-      console.log("reInit");
-      initialized = true;
-    },
-    onInit: async () => {
-      console.log("init");
-      //if (!initialized)
-      //  return;
-
-      try {
-        var url = new URL("http://127.0.0.1:3001/getRandomFood"),
-        params = {latitude:55.696233, longitude:37.570431, cardsNum:cardsNum, getTags:true}
-        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-        const response = await fetch(url, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-          },
-        });
-  
-        if (!response.ok) {
-          throw new Error(`Error! status: ${response.status}`);
-        }
-  
-        const result = await response.json();
-  
-        //console.log('result is: ', JSON.stringify(result, null, 4));
-
-        var foodCards = result["foodCards"];
-        for (var i = 0; i < foodCards.length; i++){
-          var card = foodCards[i];
-          var foodImageWithDuplicates = document.getElementsByName("image" + i.toString());
-          var foodTitleWithDuplicates = document.getElementsByName("title" + i.toString());
-          var foodDescriptionWithDuplicates = document.getElementsByName("description" + i.toString());
-          var foodUrlWithDuplicates = document.getElementsByName("url" + i.toString());
-          for (var j = 0; j < foodImageWithDuplicates.length; j++) {
-            var foodImg = foodImageWithDuplicates[j];
-            if (foodImg != null) {            
-              var imageUrl = card["imageUrl"];
-              if (!imageUrl.includes("{w}")) {
-                continue;
-              } else {
-                imageUrl = imageUrl.replace("{w}", "200");
-                imageUrl = imageUrl.replace("{h}", "200");
-                //console.log("imageUrl: ", imageUrl);
-                foodImg.src = imageUrl;  
-              }
-            }
-            var foodTitle = foodTitleWithDuplicates[j];
-            if (foodTitle != null) {
-              foodTitle.innerText = card["name"];
-            }
-            var foodDescription = foodDescriptionWithDuplicates[j];
-            if (foodDescription != null) {
-              foodDescription.innerText = card["description"];
-            }
-            var foodUrl = foodUrlWithDuplicates[j];
-            if (foodUrl != null) {
-              foodUrl.href = card["restarauntUrl"];
-            }
-          }
-        }
-      } catch (err) {
-        console.log('err: ', err)
+    onInit: () => RequestCards(maxCardsNum),
+    afterChange: index => {
+      var indexWithOffset = lastLoadedCardIdx - index;
+      if (indexWithOffset < 0) {
+        indexWithOffset = indexWithOffset + maxCardsNum;
+      }
+      if (!processingCardRequest && indexWithOffset <= leftCardsNumToRequestNew) {
+        RequestCards(cardsNumToRequest);
+        processingCardRequest = true;
       }
     }
   }
 
   const renderSlides = () =>
-  [...Array(cardsNum).keys()].map(num => (
+  [...Array(maxCardsNum).keys()].map(num => (
     <Card style={{ width: '400px', height: '400px' }}>
       <Card.Img name={"image"+num.toString()} variant="top" src="" />
       <Card.Body>
